46f115b1b7c3cc649073dfe5e7c301f69fc5607e
yurik763 <yurik763@tut.by>
2019-06-01 22:15:23 +0300

    Innit app

---
+++ app.rb
@@ -0,0 +1,88 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+
+
+#вызыается каждый раз при перезагрузке страницы любой страницы
+#before do
+#end
+
+#configure вызывается каждый раз приконфигируции приложения:
+#когда изменился код программы и перезагрузилась страница
+
+#get '/' do
+	#выбираем список постов из БД
+
+#	@results = @db.execute 'select * from Posts order by id desc'
+#	erb :index			
+#end
+
+#get '/new' do
+#  erb :new
+#end
+
+#post '/new' do
+#  @content = params[:content]
+#  @username = params[:username]
+
+#  if @content.length <= 0
+#  		@error = 'Введите текст поста'
+#  		return erb :new
+#  end
+
+  #сохранение данных в БД
+#  @db.execute 'insert into Posts (content, ceated_date, username) values (?,datetime(),?)', [@content, @username]
+
+  #перенаправление на главную страницу
+#  redirect to('/')
+#end
+
+
+#вывод информации о посте
+#get '/details/:post_id' do
+	#получаем переменную из url'a
+#	post_id = params[:post_id]
+
+	#получаем список постов(у нас будет только один пост)
+#	results = @db.execute 'select * from Posts where id = ?', [post_id]  #выбираются посты с id страницы
+	#выбираем этот один пост в переменную row
+#	@row = results[0]
+
+	#выбираем комментарий для нашего поста
+#	@comments = @db.execute 'select * from Comments where post_id = ? order by id', [post_id]
+
+	#возвращаем представление в details.erb
+#	erb :details
+#end
+
+#обработчик post-запроса /details/...  браузер отправляет данные на сервер, мы их принемае
+#post '/details/:post_id' do
+#	post_id = params[:post_id]
+#	content = params[:content]
+
+#	if content.length <= 0
+#  		@error = 'Введите текст комментария'
+#  		return erb ('/details/' + post_id)
+#  	end
+
+	#сохранение данных в БД
+#  	@db.execute 'insert into Comments 
+#  		(
+#  			content, 
+#  			ceated_date, 
+# 			post_id
+#  		) 
+# 		values 
+#  		(
+#  			?,
+#  			datetime(),
+#  			?
+#  		)', [content, post_id] # сколько занков ? столько и элементов в масиве
+			 
+#	erb "You typed comment #{content} for post #{post_id}"
+
+	#перенаправляем на страницу поста
+#	 redirect to('/details/' + post_id)
+#end
